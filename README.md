1b477c8cc93527f90e9d10334d499de84cc130ff
ego6ua <ivan4kydubun@gmail.com>
2020-03-25 15:22:02 +0100

    IK 2020-03-25

--- README.md
+++ README.md
@@ -1,24 +1,27 @@
-# CeneoScraper
+# ceneo_scraper
 ## Etap 1 - pobranie składowych pojedynczej opinii
 - opinia: li.review-box
-- identyfikator: li.review-box['data-entry-id']
+- identyfikator: li.review-box["data-entry-id"]
 - autor: div.reviewer-name-line
 - rekomendacja: div.product-review-summary > em
-- gwiazdki: span.review-score-count
+- gwiazdki: span.review-score-count 
 - potwierdzona zakupem: div.product-review-pz
-- data wystawienia: span.review-time > time['datetime'] - pierwszy element listy
-- data zakupu: span.review-time > time['datetime'] - drugi element listy
+- data wystawienia: span.review-time > time["datetime"] - pierwszy element listy
+- data zakupu: span.review-time > time["datetime"] - drugi element listy
 - przydatna: span[id=^votes-yes]
-             button.vote-yes['data-total-vote']
+             button.vote-yes["data-total-vote"]
              button.vote-yes > span
 - nieprzydatna: span[id=^votes-no]
-                button.vote-no['data-total-vote']
+                button.vote-no["data-total-vote"]
                 button.vote-no > span
 - treść: p.product-review-body
 - wady: div.cons-cell > ul
 - zalety: div.pros-cell > ul
-## Etap 2 - pobranie skladowych wszystkich opinii z pojedynczej strony
-- zapisanie skladowych opinii w zlozonej strukturze danych
-## Etap 3 - pobranie wszystkich opinii o pojedynczym produkcie
+## Etap 2 - pobranie składowych wszystkich opinii z pojedynczej strony
+- zapisanie składowych opinii w złożonej strukturze danych
+## Etap 3 - pobieranie wszystkich opinii o pojedynczym produkcie
 - przechodzenie po stronach z opiniami
-- eksport opinii do pliku (.csv. lub .xlsx lub .json)
+- eksport opinii do pliku (.csv lub .xlsx lub .json)
+## Etap 4
+- transformacja danych
+- refaktoryzacja kodu
--- scraper.py
+++ scraper.py
@@ -1,47 +1,85 @@
 #import bibliotek
 import requests
 from bs4 import BeautifulSoup
+import pprint
+import json
 
 #adres URL przykladowej strony z opiniami
-url = 'https://www.ceneo.pl/78803054#tab=reviews'
+url_prefix = "https://www.ceneo.pl"
+product_id = input("Podaj kod produktu:")
+url_postfix = "#tab=reviews"
+url = url_prefix+"/"+product_id+url_postfix
 
-#poranie kodu html strony z podanego URL
-page_respons = requests.get(url)
-page_tree = BeautifulSoup(page_respons.text, 'html.parser')
+#pusta lista na wszystkie opinie o produkcie
+opinions_list = []
 
-#wydobycie z kodu HTML strony fragmentow odpowiadajacych poszczegolnym opiniom
-opinions = page_tree.find_all("li", "js_product-review")
+while url:
+    #poranie kodu html strony z podanego URL
+    page_respons = requests.get(url)
+    page_tree = BeautifulSoup(page_respons.text, 'html.parser')
 
-#wydobycie skladowych dla pojedynczej opinii
-opinion = opinions.pop()
+    #wydobycie z kodu HTML strony fragmentow odpowiadajacych poszczegolnym opiniom
+    opinions = page_tree.find_all("li", "review-box")
+
+    
+
+    #wydobycie skladowych dla pojedynczej opinii
+    for opinion in opinions:
+        opinion_id = opinion['data-entry-id']
+        author = opinion.find("div", "reviewer-name-line").string
+        try:
+            recommendation = opinion.find("div", "product-review-summary").find("em").string
+        except AttributeError:
+            recommendation = None
+        stars = opinion.find("span", "review-score-count").string
+        try:
+            purchased = opinion.find("div", "product-review-pz").find("em").string
+        except AttributeError:
+            purchased = None
+        dates = opinion.find("span", "review-time").find_all("time")
+        review_date = dates.pop(0)["datetime"]
+        try:
+            purchase_date = dates.pop(0)["datetime"]
+        except IndexError:
+            purchase_date = None
+        useful = opinion.find("button", "vote-yes").find("span").string
+        useless = opinion.find("button", "vote-no").find("span").string
+        content = opinion.find("p", "product-review-body").get_text()
+        try:
+            pros = opinion.find("div", "pros-cell").find("ul").get_text()
+        except IndexError:
+            pros = None
+        try:
+            cons = opinion.find("div", "cons-cell").find("ul").get_text()
+        except IndexError:
+            cons = None
+
+        opinion_dict = {
+            "opinion_dict":opinion_id,
+            "recommendation":recommendation,
+            "stars":stars,
+            "content":content,
+            "author":author,
+            "pros":pros,
+            "cons":cons,
+            "useful":useful,
+            "useless":useless,
+            "purchased":purchased,
+            "purchase_date":purchase_date,
+            "review_date":review_date,
+        }
+
+        opinions_list.append(opinion_dict)
+
+    try:
+        url = url_prefix+page_tree.find("a", "pagination__next")["href"]
+    except TypeError:
+        url = None
+    print(url)
 
 
-opinion_id = opinion['data-entry-id']
-author = opinion.find("div", "reviewer-name-line").string
-recomendation = opinion.find("div", "product-review-summary").find("em").string
-stars = opinion.find("span", "review-score-count").string
-try:
-    purchased = opinion.find("div", "product-review-pz").string
-except IndexError:
-    purchased = None
-dates = opinion.find("span", "review-time").find_all("time")
-review_date = dates.pop(0)["datetime"]
-try:
-    purchase_date = dates.pop(0)["datetime"]
-except IndexError:
-    purchase_date = None
-useful = opinion.find("button", "vote-yes").find("span").string
-useless = opinion.find("button", "vote-no").find("span").string
-content = opinion.find("p", "product-review-body").get_text()
-try:
-    pros = opinion.find("div", "pros-cell").find("ul").get_text()
-except IndexError:
-    pros = None
-try:
-    cons = opinion.find("div", "cons-cell").find("ul").get_text()
-except IndexError:
-    cons = None
-
-print(opinion_id, recomendation, stars, author, pros, cons, useful, useless, purchased, purchase_date, review_date)
+with open(product_id+'.json', 'w', encoding="utf-8") as fp:
+    json.dump(opinions_list, fp, ensure_ascii=False, indent=4, separators=(',', ': '))
 
 
+# pprint.pprint(opinions_list) 
