4c3c5ea7ae034cdd2b3552726ea66b908a8b9d10
ego6ua <ivan4kydubun@gmail.com>
2020-04-01 15:09:13 +0200

    IK 2020-04-01

--- scraper.py
+++ scraper.py
@@ -67,7 +67,8 @@ while url:
     print(url)
 
 
-with open("./opinions_json/".product_id+'.json', 'w', encoding="utf-8") as fp:
+with open("./opinions_json/"+product_id+'.json', 'w', encoding="utf-8") as fp:
     json.dump(opinions_list, fp, ensure_ascii=False, indent=4, separators=(',', ': '))
 
+print(len(opinions_list))
 #pprint.pprint(opinions_list)
